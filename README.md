70961548431fdd0838cf1917366cd85936f2bea7
Yaskara Santangelo <yaskaraguimaraes@hotmail.com>
2023-09-05 16:34:22 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# origamid-react
---
+++ primeiros-passos/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+import App from './App';
+
+function App() {
+  return <h1>origamid</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
